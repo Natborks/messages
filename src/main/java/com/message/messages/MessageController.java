00c552bb61ac1c5c14f41d7856800d1d13ea7f51
natborks <nathanael@moonsquare.co>
2024-10-03 21:16:30 -0230

    Add Kafka

--- src/main/java/com/message/messages/MessageController.java
+++ src/main/java/com/message/messages/MessageController.java
@@ -1,5 +1,7 @@
 package com.message.messages;
 
+import com.message.messages.kafka.MessageProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
     private final IMessageService messageService;
 
+    @Autowired
+    private MessageProducer messageProducer;
+
     public MessageController(IMessageService messageService) {
         this.messageService = messageService;
     }
@@ -19,8 +24,11 @@ public class MessageController {
         String alteredMessage = messageService.alterMessage(message.getMessage());
 
         MessageResponse response = new MessageResponse();
+        messageProducer.sendMessage("my-topic", alteredMessage);
 
         response.setMessage(alteredMessage);
+
+        System.out.println("MY TOPIC "+  alteredMessage);
 
         return  response;
     }
